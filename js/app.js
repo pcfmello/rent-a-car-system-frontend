49952524f476a9c7e053bd4c4af6d69513e1dfa3
Paulo Cesar Ferreira de Mello <pcfmello@gmail.com>
2017-08-30 14:01:33 -0300

    Comentando o código
    - Feito comentário sobre o funcionamento de cada etapa criada.

--- js/app.js
+++ js/app.js
@@ -1,5 +1,17 @@
+// Criando um módulo com AngularJS
 var app = angular.module('app', []);
 
+// Criando um controller, com o nome ListController
 app.controller('ListController', function($scope) {
-  $scope.name = 'Hello World!';
+  
+  // Criando uma variável de escopo de view(para ser acessada na view)
+  $scope.nome = 'Hello World!';
+  
+  // Criando um objeto de escopo da view(para ser acessada na view)
+  $scope.carro = {};
+  
+  // Setando atributos no objeto criado
+  $scope.carro.marca = 'Renault';
+  $scope.carro.modelo = 'Logan';
+  
 });
