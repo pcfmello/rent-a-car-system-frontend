9c85af16ba3383f098dfcd514a97810958a71a68
Paulo Cesar Ferreira de Mello <pcfmello@gmail.com>
2017-09-11 01:07:12 -0300

    Modificada estrutura visual
    - Criada funcionalidade para verificar se existe reserva com os dados de cadastro;
    - Modificada estrutura de apresentação no mobile e desktop;
    - Incluído ícones nos botões de ação;

--- js/controllers/cadastroController.js
+++ js/controllers/cadastroController.js
@@ -1,7 +1,12 @@
 var app = angular.module('app');
 
 app.controller('CadastroController', function($scope, $location, $routeParams, $http) {
+
+  $scope.locais = [];
+  $scope.reservaJaExiste = false;
   var idReserva = $routeParams.id // ID enviado por quem requisitou a rota
+
+  // Verifica se existe cadastro para editar ou cria um novo cadastro
   if(idReserva) {
     // Requisição AJAX para obter a reserva pelo ID
     $http.get('http://localhost:5000/reservas/' + idReserva)
@@ -14,8 +19,6 @@ app.controller('CadastroController', function($scope, $location, $routeParams, $
     $scope.reserva = {};
   }
 
-  $scope.locais = [];
-
   // Requisição AJAX para obter a lista de filiais do backend
   $http.get('http://localhost:5000/locais')
     .then(function(response) {
@@ -24,18 +27,36 @@ app.controller('CadastroController', function($scope, $location, $routeParams, $
 
   // Requisição AJAX para salvar ou atualizar a reserva do backend
   $scope.salvar = function salvar(formularioEhValido) {
-    if(!formularioEhValido) return;
-    if($scope.reserva._id) {
-      $http.put('http://localhost:5000/reservas', $scope.reserva)
-        .then(function(response) {
-            $location.path('/');
-        });
-    } else {
-      $http.post('http://localhost:5000/reservas', $scope.reserva)
-        .then(function(response) {
-          $location.path('/');
+
+    // Consulta se há reservas com esse local, veículo e entre as datas e horários
+    $http.post('http://localhost:5000/reservas/consulta', {
+        local: $scope.reserva.local._id,
+        carro: $scope.reserva.carro,
+        dataInicio: $scope.reserva.dataInicio,
+        dataFim: $scope.reserva.dataFim
+      })
+      .then(function(response) {
+        $scope.reservaJaExiste = !!response.data.length;
+        // Se reserva está disponível, cria/altera uma reserva
+        if(!$scope.reservaJaExiste) {
+          if(!formularioEhValido) return;
+          if($scope.reserva._id) {
+            $http.put('http://localhost:5000/reservas', $scope.reserva)
+            .then(function(response) {
+              $location.path('/');
+            });
+          } else {
+            $http.post('http://localhost:5000/reservas', $scope.reserva)
+            .then(function(response) {
+              $location.path('/');
+            });
+          }
+        } else {
+          console.log("Reserva já existe");
+        }
       });
-    }
+
+
   }
 
   $scope.cancelar = function cancelar() {
